68bcc4ae32a2d1c9efc8474161eae96b59dee901
Ryan Neufeld <rkneufeld@gmail.com>
2009-06-04 09:07:02 -0500

    added a little printout to install.rb

--- install.rb
+++ install.rb
@@ -1,7 +1,8 @@
 # Install hook code here
+puts "Installing ActsAsFreetaggable...\n"
 
 # Make symlinks for acts_as_category and has_many_polymorphs unless they exist
 Rake::Task["freetaggable:symlink_plugins"]
 
-# Generate db migration for tag and taggings? 
+# Generate db migration for tag and taggings?
 Rake::Task["freetaggable:copy_migrations"]
