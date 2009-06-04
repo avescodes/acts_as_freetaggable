e268a97545d2083e0a5545892f0f876f15c2886a
Ryan Neufeld <neufelry@gmail.com>
2009-06-04 11:16:45 -0500

    Added creation of db/migrate directory if it does not exist

--- install.rb
+++ install.rb
@@ -17,6 +17,7 @@ end
 
 # Generate db migration for tag and taggings?
 puts "Copy migrations from the ActsAsFreetaggable plugin to your local rails migrations folder"
+FileUtils.mkdir_p "#{RAILS_ROOT}/db/migrate"
 Dir["#{File.dirname(__FILE__)}/db/migrate/*.rb"].each do |migration_path|
   migration = migration_path.split('/').last
   new_path = "#{RAILS_ROOT}/db/migrate/#{migration}"
